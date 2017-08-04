1b6e1471949d4bad3c2b7135ac36258ae9adf514
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-08-04 23:07:51 +1000

    Correct Rubocop offenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 Product.create!(
-  name: 'Demo product 5000'
+  name: 'Demo product 5000',
   slug: 'demo-product-5000',
   language: 'en',
-  image_url: 'https://cdn.interflux.com/images/products/demo-product-5000-1.jpg'
-  teaser: 'Teaser lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  image_url: 'https://cdn.interflux.com/images/products/demo-product-5000-1.jpg',
+  teaser: 'Teaser lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   corpus: 'Corpus lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 )
 
 Product.create!(
-  name: 'Demo product 5000'
+  name: 'Demo product 5000',
   slug: 'demo-product-5000',
   language: 'it',
-  image_url: 'https://cdn.interflux.com/images/products/demo-product-5000-1.jpg'
-  teaser: 'Teaser lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  image_url: 'https://cdn.interflux.com/images/products/demo-product-5000-1.jpg',
+  teaser: 'Teaser lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   corpus: 'Corpus lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 )
 
 Product.create!(
-  name: 'Demo product 123X'
+  name: 'Demo product 123X',
   slug: 'demo-product-123X',
   language: 'en',
-  image_url: 'https://cdn.interflux.com/images/products/demo-product-123X-1.jpg'
-  teaser: 'Teaser lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  image_url: 'https://cdn.interflux.com/images/products/demo-product-123X-1.jpg',
+  teaser: 'Teaser lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
   corpus: 'Corpus lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 )
