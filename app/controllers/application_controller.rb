58b900a35941fc2fa3416b86bf07f26f2d2d92e5
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-10-17 23:56:25 +1100

    Remove unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < JSONAPI::ResourceController
-  # include AuthorizeApiRequest
 
-  # protect_from_forgery with: :null_session
-  # before_action :authenticate_request
-  # attr_reader :current_user
-
-  # prepend SimpleCommand
-
-  # private
-
-  # def authenticate_request
-  #   # @current_user = AuthorizeApiRequest.call(request.headers).result
-  #   @current_user = call(request.headers).result
-  #   render json: { error: 'Not Authorized' }, status: 401 unless @current_user
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Authenticated routes
   post 'authenticate', to: 'authentication#authenticate'
   namespace :admin do
-    jsonapi_resources :products
     jsonapi_resources :documents
+    jsonapi_resources :products
+    jsonapi_resources :user, only: %i[show]
   end
-  # Public routes
   namespace :public do
     jsonapi_resources :products, only: %i[index show]
     jsonapi_resources :documents, only: %i[index show]
