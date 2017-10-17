e7d39f86d6eb695bb416ebbf2b52e7626fbd7a1a
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-10-17 21:34:47 +1100

    Add authentication tests

--- app/controllers/admin/authenticated_controller.rb
+++ app/controllers/admin/authenticated_controller.rb
@@ -12,26 +12,26 @@ module Admin
       return missing_header if request.headers['Authorization'].nil?
       token = JsonWebToken.decode(request.headers['Authorization'].split(' ').last)
       return invalid_token if token.nil?
-      user = User.find(token['user_id'])
+      user = User.find_by(id: token['user_id'])
       return missing_user if user.nil?
 
       # TODO: return no_permission if user.can_use_admin
     end
 
     def missing_header
-      render status: 401, json: { error: 'No Authorization field is included in the HTTP header' }
+      render status: 401, json: { error: 'The header of your request is missing the Authorization field' }
     end
 
     def invalid_token
-      render status: 401, json: { error: 'Invalid JWT authentication token' }
+      render status: 401, json: { error: 'Your JWT token is either invalid or expired' }
     end
 
     def missing_user
-      render status: 401, json: { error: 'No user found for authentication token' }
+      render status: 401, json: { error: 'No user was found for your authentication token' }
     end
 
-    # def no_permission
-    #   render status: 401, json: { error: 'User does not have permission' }
-    # end
+    def no_permission
+      render status: 401, json: { error: 'User does not have permission' }
+    end
   end
 end
