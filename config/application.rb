c8b21e66dd53dc67d4daff25533ecd2bfd3bffc2
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-10-30 22:25:58 +1100

    Add patch and delete to CORS accepted methods

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module ApiInterfluxCom
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
       end
     end
   end
