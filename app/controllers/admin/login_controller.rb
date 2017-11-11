5c86a09c7fa635edc8cafee7ceb539c489fab032
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-11-11 18:44:58 +1100

    Add admin login controller

---
+++ app/controllers/admin/login_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Admin
+  class LoginController < ApplicationController
+
+    def authenticate
+      byebug
+      user = User.find_by(email: params[:email])
+      return invalid_login unless user && user.authenticate(params[:password])
+      JsonWebToken.encode(user_id: user.id)
+      render json: { auth_token: JsonWebToken.encode(user_id: user.id) }
+    end
+
+    private
+
+    def user_params
+      params.require(:authentication).permit(:email, :password)
+    end
+
+    def invalid_login
+      render status: 401, json: {
+        errors: [{
+          status: '401',
+          code: 'invalid-login',
+          detail: 'Your login credentials are invalid.'
+        }]
+      }
+    end
+  end
+end
