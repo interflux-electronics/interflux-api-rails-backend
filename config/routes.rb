3784caca7bc61689356853b388a77202689e6cd6
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-11-11 18:44:44 +1100

    Remove document controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  post 'authenticate', to: 'authentication#authenticate'
   namespace :admin do
-    jsonapi_resources :documents
+    post 'login', to: 'login#authenticate'
     jsonapi_resources :products
-    jsonapi_resources :user, only: %i[show]
+    # jsonapi_resources :documents
+    # jsonapi_resources :user, only: %i[show]
   end
   namespace :public do
     jsonapi_resources :products, only: %i[index show]
-    jsonapi_resources :documents, only: %i[index show]
+    # jsonapi_resources :documents, only: %i[index show]
   end
 end
