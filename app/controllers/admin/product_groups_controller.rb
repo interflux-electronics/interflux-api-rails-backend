6a3dc055aaccb69b630ccaa097081c7bd6b3f0e6
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-11-28 21:20:36 +1100

    Add controllers for product groups

---
+++ app/controllers/admin/product_groups_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Admin
+  class ProductGroupsController < Admin::AuthenticatedController
+
+    # GET /admin/product_groups
+    def index
+      @records = ProductGroup.all
+      @resources = @records.map { |record| ProductGroupResource.new(record, nil) }
+      render json: JSONAPI::ResourceSerializer.new(Admin::ProductGroupResource).serialize_to_hash(@resources)
+    end
+
+  end
+end
---
+++ app/controllers/admin/product_sub_groups_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Admin
+  class ProductSubGroupsController < Admin::AuthenticatedController
+
+    # GET /admin/product_sub_groups
+    def index
+      @records = ProductSubGroup.all
+      @resources = @records.map { |record| ProductSubGroupResource.new(record, nil) }
+      render json: JSONAPI::ResourceSerializer.new(Admin::ProductSubGroupResource).serialize_to_hash(@resources)
+    end
+
+  end
+end
