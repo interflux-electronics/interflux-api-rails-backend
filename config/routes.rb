98f7b8326310633333ebf0ada8a2987ebbac398c
Jan Werkhoven <jan.werkhoven@gmail.com>
2017-11-28 21:19:32 +1100

    Add product groups to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :admin do
     post 'login', to: 'login#authenticate'
     jsonapi_resources :products
+    jsonapi_resources :product_groups, only: %i[index]
+    jsonapi_resources :product_sub_groups, only: %i[index]
     # jsonapi_resources :documents
     # jsonapi_resources :user, only: %i[show]
   end
