e29fc4d3a0f661e1d71fa249358bce798c52677e
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-03-24 23:27:13 +1100

    Improve test naming

--- test/integration/admin/authentication_test.rb
+++ test/integration/admin/authentication_test.rb
@@ -4,10 +4,11 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
   def setup
     @casual_user = users('casual_user')
     @casual_user_token = JsonWebToken.encode(user_id: @casual_user.id)
+    @product = products('IF_2005M')
   end
 
-  test 'Authenticated routes throw 401 missing_header errors' do
-    get "/admin/products/#{products('IF_2005M').id}"
+  test 'Authenticated routes returns 401 missing-header errors' do
+    get "/admin/products/#{@product.id}"
     assert_response 401
     first_error = JSON.parse(@response.body)['errors'][0]
     assert_equal first_error['status'], '401'
@@ -15,8 +16,8 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
     assert_equal first_error['detail'], 'The header of your request is missing the Authorization field.'
   end
 
-  test 'Authenticated routes throw 401 invalid_token errors' do
-    get "/admin/products/#{products('IF_2005M').id}", headers: { 'Authorization': 'bogus-token' }
+  test 'Authenticated routes returns 401 invalid-token errors' do
+    get "/admin/products/#{@product.id}", headers: { 'Authorization': 'bogus-token' }
     assert_response 401
     first_error = JSON.parse(@response.body)['errors'][0]
     assert_equal first_error['status'], '401'
@@ -24,8 +25,8 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
     assert_equal first_error['detail'], 'Your JWT token is either invalid or expired.'
   end
 
-  test 'Authenticated routes throw 401 missing_user errors' do
-    get "/admin/products/#{products('IF_2005M').id}", headers: { 'Authorization': JsonWebToken.encode(user_id: 'unknown-ID') }
+  test 'Authenticated routes returns 401 missing-user errors' do
+    get "/admin/products/#{@product.id}", headers: { 'Authorization': JsonWebToken.encode(user_id: 'unknown-ID') }
     assert_response 401
     first_error = JSON.parse(@response.body)['errors'][0]
     assert_equal first_error['status'], '401'
@@ -33,8 +34,8 @@ class AuthenticationTest < ActionDispatch::IntegrationTest
     assert_equal first_error['detail'], 'No user was found for your authentication token.'
   end
 
-  test 'Authenticated routes throw 401 no_permission errors' do
-    get "/admin/products/#{products('IF_2005M').id}", headers: { 'Authorization': @casual_user_token }
+  test 'Authenticated routes returns 401 no-permission errors' do
+    get "/admin/products/#{@product.id}", headers: { 'Authorization': @casual_user_token }
     assert_response 401
     first_error = JSON.parse(@response.body)['errors'][0]
     assert_equal first_error['status'], '401'
