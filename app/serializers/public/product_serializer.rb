5b3b22ea26e690c9061292dba8d6e0894112d402
Jan Werkhoven <jan.werkhoven@gmail.com>
2018-04-22 22:18:54 +1000

    Add public product and product category serializers

---
+++ app/serializers/public/product_serializer.rb
@@ -0,0 +1,12 @@
+module Public
+  class ProductSerializer < ApplicationSerializer
+    attributes :name,
+               :slug,
+               :pitch,
+               :body,
+               :public
+
+    belongs_to :main_category, record_type: :product_category
+    belongs_to :sub_category, record_type: :product_category
+  end
+end
