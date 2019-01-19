f6898163569a04e2184e99935c07bd3e8eade52e
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-01-19 17:38:19 +1100

    Rewrite the Puma configs

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,19 +1,22 @@
+#!/usr/bin/env puma
+
+app_dir = File.expand_path('../../../../', __dir__)
+server_dir = "/var/www/api.interflux.com/server"
+
 environment 'production'
 
-# directory File.expand_path('../../../../', __dir__)
+workers 2
+threads 0, 5
 
-directory '/var/www/api.interflux.com/releases/latest'
+bind 'tcp://0.0.0.0:3000'
+bind "unix://#{server_dir}/sockets/puma.sock"
 
-# The socket from which to serve the Rails app
-bind 'unix:///var/www/api.interflux/server/puma.sock'
-pidfile '/var/www/api.interflux/server/puma.pid'
-state_path '/var/www/api.interflux/server/puma.state'
-stdout_redirect '/var/www/api.interflux/server/puma.stdout.log', 'server/puma.stderr.log', true
-activate_control_app 'unix:///var/www/api.interflux/server/pumactl.sock', no_token: true
+pidfile "#{server_dir}/pids/puma.pid"
+state_path "#{server_dir}/pids/puma.state"
+stdout_redirect "#{server_dir}/log/puma.stdout.log", "#{server_dir}/log/puma.stderr.log", true
+activate_control_app "unix://#{server_dir}/sockets/pumactl.sock", no_token: true
 
 daemonize true
-
-# TODO: Investigate phased restart to avoid loss of Active Jobs
 
 preload_app!
 
@@ -21,12 +24,7 @@ before_fork do
   ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
 end
 
-on_restart do
-  puts 'On restart...'
-end
-
 on_worker_boot do
-  puts 'On worker boot...'
   ActiveSupport.on_load(:active_record) do
     ActiveRecord::Base.establish_connection
   end
