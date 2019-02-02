d327bef058d58bdd64a028a14238537996228536
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-02-02 18:30:06 +1100

    Improve the scaffold generator script

--- lib/generators/custom_scaffold/custom_scaffold_generator.rb
+++ lib/generators/custom_scaffold/custom_scaffold_generator.rb
@@ -10,6 +10,12 @@ class CustomScaffoldGenerator < Rails::Generators::NamedBase
 
   source_root File.expand_path('templates', __dir__)
 
+  def init
+    verify_inputs
+  end
+
+  private
+
   def verify_inputs
     puts '---------'
     puts 'Generating scaffold...'
@@ -24,17 +30,21 @@ class CustomScaffoldGenerator < Rails::Generators::NamedBase
     return puts "ERROR: Please supply a scope with '--scope=admin'" if @scope.nil?
     return puts "ERROR: The scope can only be 'Admin' or 'Public'" unless %w[Admin Public].include? @scope.classify
     return puts 'ERROR: Please only use snake_case for the resource name (e.g.: product_features).' if /[A-Z\- ]/.match(@name).present?
+
+    generate_files
+  end
+
+  def generate_files
     puts '---------'
     template 'migration.rb.tt', "db/migrate/#{timestamp}_create_#{name.downcase.pluralize}.rb"
     template 'model.rb.tt', "app/models/#{name.downcase.singularize}.rb"
     template 'controller.rb.tt', "app/controllers/#{@version}/#{@scope}/#{name.downcase.pluralize}_controller.rb"
     template 'serializer.rb.tt', "app/serializers/#{@version}/#{@scope}/#{name.downcase.singularize}_serializer.rb"
     template 'fixtures.yml.tt', "test/fixtures/#{name.downcase.pluralize}.yml"
-    template 'test.rb.tt', "test/integration/#{@version}/#{@scope}/#{name.downcase.pluralize}_test.rb"
+    template 'test.rb.tt', "test/integration/#{@version}/#{@scope}/#{name.downcase.pluralize}_integration_test.rb"
     prepend_to_file "#{Rails.root}/config/routes.rb" do
       str = "# TODO: resources :#{name.downcase.pluralize}"
       str << ", path: '/#{name.downcase.pluralize.dasherize}'" if str.include? '_'
-      str << "\n"
       str << "\n"
       str
     end
@@ -42,8 +52,6 @@ class CustomScaffoldGenerator < Rails::Generators::NamedBase
     puts 'Success!'
     puts '---------'
   end
-
-  private
 
   def timestamp
     Time.now.utc.strftime('%Y%m%d%H%M%S')
