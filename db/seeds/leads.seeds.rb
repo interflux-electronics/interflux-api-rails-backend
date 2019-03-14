84a61854cde6686c5ccdecd76a6021d828ff483e
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-03-14 12:52:08 +1100

    Rubocop the seed files

--- db/seeds/leads.seeds.rb
+++ db/seeds/leads.seeds.rb
@@ -3,54 +3,52 @@ require 'byebug'
 require 'ap'
 
 after :countries do
+  count_before = Lead.count
 
-count_before = Lead.count
+  puts '---------'
+  puts 'Seeding leads'
+  puts '---------'
 
-puts '---------'
-puts 'Seeding leads'
-puts '---------'
+  file = File.read 'db/seeds/data/leads.yml'
+  data = YAML.safe_load(file)
 
-file = File.read 'db/seeds/data/leads.yml'
-data = YAML.safe_load(file)
+  data.each_with_index do |_lead, i|
+    lead = OpenStruct.new(_lead)
 
-data.each_with_index do |_lead, i|
-  lead = OpenStruct.new(_lead)
+    puts "#{i + 1} - #{lead.email}"
 
-  puts "#{i + 1} - #{lead.email}"
+    record = Lead.find_by(email: lead.email)
 
-  record = Lead.find_by(email: lead.email)
+    properties = OpenStruct.new(
+      name: lead.name,
+      company: lead.company,
+      email: lead.email,
+      mobile: lead.mobile,
+      message: lead.message,
+      purpose: lead.purpose,
+      source: lead.source,
+      country_id: Country.where(iso_3161_1_alpha_2_code: lead.country_id).first.id,
+      ip_country_id: Country.where(iso_3161_1_alpha_2_code: lead.ip_country_id).first.id,
+      ip_region: lead.ip_region,
+      ip_city: lead.ip_city,
+      ip: lead.ip
+    )
 
-  properties = OpenStruct.new(
-    name: lead.name,
-    company: lead.company,
-    email: lead.email,
-    mobile: lead.mobile,
-    message: lead.message,
-    purpose: lead.purpose,
-    source: lead.source,
-    country_id: Country.where(iso_3161_1_alpha_2_code: lead.country_id).first.id,
-    ip_country_id: Country.where(iso_3161_1_alpha_2_code: lead.ip_country_id).first.id,
-    ip_region: lead.ip_region,
-    ip_city: lead.ip_city,
-    ip: lead.ip
-  )
-
-  if record.nil?
-    Lead.create!(properties.to_h)
-  else
-    record.update!(properties.to_h)
+    if record.nil?
+      Lead.create!(properties.to_h)
+    else
+      record.update!(properties.to_h)
+    end
   end
-end
 
-puts '---------'
-count_after = Lead.count
-difference = count_after - count_before
-puts "Before seeding, the database had #{count_before} leads."
-puts "After seeding, the database has #{count_after}."
-puts "That's #{difference} new ones."
-puts '---------'
-puts 'Success!'
-puts '---------'
-puts ' '
-
+  puts '---------'
+  count_after = Lead.count
+  difference = count_after - count_before
+  puts "Before seeding, the database had #{count_before} leads."
+  puts "After seeding, the database has #{count_after}."
+  puts "That's #{difference} new ones."
+  puts '---------'
+  puts 'Success!'
+  puts '---------'
+  puts ' '
 end
