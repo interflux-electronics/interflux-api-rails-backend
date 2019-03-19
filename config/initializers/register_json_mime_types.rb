46cd9928178cd9d36ce8459ba1b624b0755ceb26
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-03-19 13:19:41 +1100

    Fix issue where controllers can't read POST json if vnd.api+json

--- config/initializers/register_json_mime_types.rb
+++ config/initializers/register_json_mime_types.rb
@@ -2,8 +2,13 @@
 # type `application/vnd.api+json`. We do this to comply with the contract as
 # read here: http://jsonapi.org/format/#content-negotiation-servers
 # Inspired from: https://blog.codeship.com/the-json-api-spec/
+# See also: https://github.com/rails-api/active_model_serializers/issues/1027
 
-api_mime_types = %w[
+api_mime_types = %W(
   application/vnd.api+json
-]
+  text/x-json
+  application/json
+)
+
+Mime::Type.unregister :json
 Mime::Type.register 'application/vnd.api+json', :json, api_mime_types
