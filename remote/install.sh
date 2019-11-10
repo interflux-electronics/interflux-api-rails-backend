2436b328ea621598b5ead9c2018d0aa92be0b171
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-11-10 21:36:05 +1100

    Stop and restart Puma on deploy

--- remote/install.sh
+++ remote/install.sh
@@ -84,8 +84,10 @@ and echo ----------
 # echo bin/pumactl -F config/puma/production.rb -T '12345' start
 # bin/pumactl -F config/puma/production.rb -T '12345' start
 and echo Restart the Puma server!
-and echo bin/pumactl -F config/puma/production.rb -T '12345' phased-restart
-and bin/pumactl -F config/puma/production.rb -T '12345' phased-restart
+and echo bin/pumactl -F config/puma/production.rb -T '12345' start
+and bin/pumactl -F config/puma/production.rb -T '12345' start
+# and echo bin/pumactl -F config/puma/production.rb -T '12345' restart
+# and bin/pumactl -F config/puma/production.rb -T '12345' restart
 # and echo bin/pumactl -F config/puma/production.rb -T '12345' phased-restart
 # and bin/pumactl -F config/puma/production.rb -T '12345' phased-restart
 # echo "bin/pumactl -C 'unix:///var/www/api.interflux.com/server/sockets/pumactl.sock' -T '12345' phased-restart"
