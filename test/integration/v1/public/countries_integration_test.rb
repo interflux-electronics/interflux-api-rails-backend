fe1718fe3abca3fe408e4dd642ef2cc02033816c
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-11-18 08:26:51 +1100

    Write test for country controller

---
+++ test/integration/v1/public/countries_integration_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+module V1
+  module Public
+    class ProductIntegrationTest < ActionDispatch::IntegrationTest
+      def setup
+        @header = public_header
+      end
+
+      test 'can fetch all' do
+        get '/v1/public/countries', headers: @header
+        assert_response 200
+        json = JSON.parse(@response.body)
+        assert_equal 2, json['data'].length
+      end
+    end
+  end
+end
