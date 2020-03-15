1db5c92952a6ed0dc7ad2f6ab2f58dfee4f6de02
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-03-15 16:13:42 +1100

    Update country serialisers

--- app/serializers/v1/public/country_serializer.rb
+++ app/serializers/v1/public/country_serializer.rb
@@ -3,21 +3,11 @@ module V1
     class CountrySerializer < ApplicationSerializer
       attributes :name_english,
                  :name_native,
-                 # :region,
-                 # :subregion,
                  :two_letter_code,
                  :three_letter_code,
                  :numeric_code,
-                 # :region,
-                 # :subregion,
-                 # :latitude,
-                 # :longitude,
                  :area,
                  :population
-                 # :flag_url,
-                 # :timezones,
-                 # :top_level_domains,
-                 # :calling_codes
     end
   end
 end
