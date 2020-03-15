12013a3dfbf277ecd8558a332f3c632732c2d683
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-03-15 16:17:06 +1100

    Re-annotate models

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,8 +6,8 @@
 #  path       :string
 #  sizes      :string
 #  formats    :string
-#  tag_long   :string
-#  tag_short  :string
+#  caption    :string
+#  alt        :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
