37733073331907030c44e268b277dad26f0b0d33
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-03-16 17:07:09 +1100

    Serialise more person fields

--- app/serializers/v1/admin/person_serializer.rb
+++ app/serializers/v1/admin/person_serializer.rb
@@ -2,7 +2,11 @@ module V1
   module Admin
     class PersonSerializer < ApplicationSerializer
       attributes :first_name,
-                 :last_name
+                 :last_name,
+                 :chinese_name,
+                 :email,
+                 :phone,
+                 :male
     end
   end
 end
