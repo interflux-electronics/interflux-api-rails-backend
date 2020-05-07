ba09718f3639a4428d73f6f1a1dd0e18ac726fee
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-05-07 11:49:29 +1000

    Improve product serialiser

--- app/serializers/v1/public/product_serializer.rb
+++ app/serializers/v1/public/product_serializer.rb
@@ -13,12 +13,16 @@ module V1
       belongs_to :product_family
       belongs_to :image
 
+      # belongs_to :image, if: Proc.new { |record, params|
+      #   params && params['include'] && ( params['include'].split(',').include?('image') || params['include'].split(',').include?('products.image') )
+      # }
+
       has_many :documents, if: Proc.new { |record, params|
         params && params['include'] && params['include'].split(',').include?('documents')
       }
 
       has_many :images, if: Proc.new { |record, params|
-        params && params['include'] && ( params['include'].split(',').include?('image') || params['include'].split(',').include?('products.image') )
+        params && params['include'] && ( params['include'].split(',').include?('images') )
       }
 
       has_many :features, if: Proc.new { |record, params|
