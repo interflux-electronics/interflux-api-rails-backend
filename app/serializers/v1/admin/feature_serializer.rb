8e4f6bd62a6c514df4cbc138b147897ef649cb96
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-08-12 14:03:58 +1000

    Allow create and update of features

--- app/serializers/v1/admin/feature_serializer.rb
+++ app/serializers/v1/admin/feature_serializer.rb
@@ -1,17 +1,16 @@
 module V1
   module Admin
     class FeatureSerializer < ApplicationSerializer
-      # attributes :slug,
-      #            :name,
-      #            :public
+      attributes :slug,
+                 :text,
+                 :icon,
+                 :gist,
+                 :category,
+                 :has_page
 
-      # belongs_to :thing
-      # belongs_to :main_group, record_type: :product_group, serializer: :product_group
-      # belongs_to :sub_group, record_type: :product_group, serializer: :product_group
-
-      # has_many :things
-      # has_many :related_products, record_type: :product, serializer: :related_products
-      # has_many :related_articles, record_type: :article, serializer: :related_articles
+      has_many :products, if: Proc.new { |record, params|
+        params && params['include'] && params['include'].split(',').include?('products')
+      }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
       resources :countries
       resources :documents
       resources :document_categories, path: '/document-categories'
+      resources :features
       resources :images
       resources :languages
       resources :leads
