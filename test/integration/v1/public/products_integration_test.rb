3d137c32eb7d5b02d19b693294352f5e1bf5742a
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-02-21 09:28:26 +1100

    Add voluntary failing test to test Github CI flow

--- test/integration/v1/public/products_integration_test.rb
+++ test/integration/v1/public/products_integration_test.rb
@@ -17,7 +17,7 @@ module V1
         json = JSON.parse(@response.body)
 
         # Should return 4 out of 5 products (should filter out public=false)
-        assert_equal 5, json['data'].length
+        assert_equal 6, json['data'].length
       end
 
       test 'can fetch one by slug' do
