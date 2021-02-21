4a5ff4b16f85274fc6f4a051ab43de4aee37a1e2
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-02-21 12:46:10 +1100

    Improve CD deploy script 17

--- remote/deploy.sh
+++ remote/deploy.sh
@@ -6,15 +6,19 @@ set -o pipefail
 user=bot
 server=server.interflux.com
 domain=api.interflux.com
+branch=$(git rev-parse --abbrev-ref HEAD)
+revision=$(git rev-parse --short HEAD)
 
 echo "----------"
 echo "Deploying:"
 echo $domain
 echo $user@$server
+echo $branch
+echo $revision
 echo "----------"
 
 (
   set -x
   scp -i ~/.ssh/$user@$server remote/deploy-remote.sh $user@$server:~/
-  ssh -i ~/.ssh/$user@$server $user@$server "~/deploy-remote.sh $domain; rm -f ~/deploy-remote.sh"
+  ssh -i ~/.ssh/$user@$server $user@$server "~/deploy-remote.sh $domain $branch $revision; rm -f ~/deploy-remote.sh"
 )
