2f2ecb0ed917a6c63a6ff95e8d725099671d4209
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-04-05 13:19:48 +1000

    Add installers to backup command

--- remote/backup-remote.sh
+++ remote/backup-remote.sh
@@ -7,8 +7,10 @@ timestamp=$1
 
 (
   set -x
-
   cd /var/www/api.interflux.com/builds/production/latest/
+  rbenv install -s
+  gem install bundler
+  bin/bundle install
   export RAILS_ENV=production
   bin/rails db:data:dump_dir dir=$timestamp
   mv db/$timestamp/ /var/www/api.interflux.com/db/production-$timestamp/
