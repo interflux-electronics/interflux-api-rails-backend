ff627a9038b226a150d41dbe0382e51061583114
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-04-05 13:39:07 +1000

    Unbreak family.products

--- app/models/product_family.rb
+++ app/models/product_family.rb
@@ -15,6 +15,8 @@
 class ProductFamily < ApplicationRecord
   belongs_to :product_family, optional: true
 
+  has_many :products
+
   has_many :product_family_images
   has_many :images, through: :product_family_images, source: :image
 end
