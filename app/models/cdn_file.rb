307ffa2e8878d1b910361b976c00e3ed8c68d112
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-04-09 10:04:43 +1000

    Add after_create method for CDN files

--- app/models/cdn_file.rb
+++ app/models/cdn_file.rb
@@ -1,4 +1,3 @@
-# == Schema Information
 #
 # Table name: cdn_files
 #
@@ -19,4 +18,70 @@ class CdnFile < ApplicationRecord
   scope :fonts, -> { where "path LIKE :prefix", prefix: "fonts/%" }
   scope :images, -> { where "path LIKE :prefix", prefix: "images/%" }
   scope :documents, -> { where "path LIKE :prefix", prefix: "documents/%" }
+
+  after_create
+
+  # To make full name searches possible we update the full name column after every save
+  after_create :create_parent_record
+
+  private
+
+  def create_parent_record
+    puts "creating parent record for: #{path}"
+
+    is_valid_image_path = [
+      'images/products/',
+      'images/processes/',
+      'images/logos/',
+      'images/icons/'
+    ].any? { |x| path.start_with?(x) }
+
+    if is_valid_image_path
+      puts 'is image path'
+
+      # Fetch all CDN file records from database
+      cdn_files = CdnFile.all
+
+      # Find the shared path for images. The below example should result in only 2 image records
+      # being created with a range of size and type variaties.
+      # logo.svg
+      # logo.png
+      # photo@800x800.jpg
+      # photo@800x800.webp
+      # photo@1200x1200.jpg
+      # photo@1200x1200.webp
+      shared_path = if path.include? '@' then path.split('@').first else path.split('.').first end
+
+      subset = cdn_files.filter { |x| x.path.start_with?("#{shared_path}@") } if path.include? '@'
+      subset = cdn_files.filter { |x| x.path.start_with?("#{shared_path}.") } if path.exclude? '@'
+
+      variations = subset.map { |x| "@#{x.path.split('@').last}" } .sort_by { |x| x.split('x').first.to_i } .join(',') if path.include? '@'
+      variations = subset.map { |x| ".#{x.path.split('.').last}" } .join(',') if path.exclude? '@'
+
+      props = OpenStruct.new(
+        path: shared_path,
+        alt: shared_path.split('/').last.gsub('-', ' '),
+        variations: variations
+      )
+
+      image = Image.find_by(path: shared_path)
+
+      if image.present?
+        puts "UPDATED image record"
+        image.update!(props.to_h)
+      else
+        puts "CREATED image record"
+        image = Image.create!(props.to_h)
+      end
+
+      subset.each do |cdn_file|
+        cdn_file.image_id = image.id
+        cdn_file.save!
+      end
+    end
+
+    if path.starts_with? 'documents/'
+      puts 'is document path'
+    end
+  end
 end
