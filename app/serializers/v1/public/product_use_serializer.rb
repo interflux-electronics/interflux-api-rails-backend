f9a7f8adcf4069c00d6a48c8b8836db9accc5d19
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-12-19 20:16:49 +1100

    Serialise show_alternative_avatar to public

--- app/serializers/v1/public/product_use_serializer.rb
+++ app/serializers/v1/public/product_use_serializer.rb
@@ -3,10 +3,12 @@ module V1
     class ProductUseSerializer < ApplicationSerializer
       attributes :rank_among_products,
                  :rank_among_uses,
-                 :show_on_product_list
+                 :show_on_product_list,
+                 :show_alternative_avatar
 
       belongs_to :product
       belongs_to :use
+      belongs_to :image
     end
   end
 end
