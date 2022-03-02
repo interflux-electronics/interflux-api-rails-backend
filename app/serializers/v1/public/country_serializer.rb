5067696103906626e49509f5204c918aa030c98b
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-03-02 16:54:16 +0100

    No longer serialise all properties of Countries to Public

--- app/serializers/v1/public/country_serializer.rb
+++ app/serializers/v1/public/country_serializer.rb
@@ -1,24 +1,10 @@
 module V1
   module Public
     class CountrySerializer < ApplicationSerializer
-      attributes :id,
-                 :name_english,
+      attributes :name_english,
                  :name_native,
-                 :two_letter_code,
-                 :three_letter_code,
-                 :numeric_code,
-                 :region,
-                 :subregion,
                  :latitude,
-                 :longitude,
-                 :area,
-                 :population,
-                 :flag_url,
-                 :timezones,
-                 :top_level_domains,
-                 :calling_codes,
-                 :created_at,
-                 :updated_at
+                 :longitude
     end
   end
 end
