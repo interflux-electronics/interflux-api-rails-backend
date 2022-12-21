d5b46588d368449b9266dba4894b7bc1393508a5
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-12-21 18:07:00 +1000

    Add generated test files for translations

---
+++ test/integration/v1/public/translation_events_integration_test.rb
@@ -0,0 +1,9 @@
+# require 'test_helper'
+# 
+# module V1
+#   module Public
+#     class TranslationEventIntegrationTest < ApplicationController
+# 
+#     end
+#   end
+# end
---
+++ test/integration/v1/public/translations_integration_test.rb
@@ -0,0 +1,9 @@
+# require 'test_helper'
+# 
+# module V1
+#   module Public
+#     class TranslationIntegrationTest < ApplicationController
+# 
+#     end
+#   end
+# end
