46a51441d5d5d86828f3e0a63367c439d9657106
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-02-04 12:14:10 +1100

    Log all errors by action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Log mailer errors to specified file.
+  # https://stackoverflow.com/questions/72480944/rails-how-to-configure-the-logger-for-the-rails-action-mailer
+  config.action_mailer.logger = ActiveSupport::Logger.new('/var/www/api.interflux.com/logs/mailer.log')
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
