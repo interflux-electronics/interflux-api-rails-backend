c526306034b216650a2e2a30904b59e5d63b29e7
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-08-21 08:12:29 +1000

    Add after_create hook to Image for starting conversion

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,14 @@
 class Image < ApplicationRecord
+  # key :path, String, ID
+  # key :original, String
+  # key :variations, String
+  # key :converting, Boolean
+  # key :conversion_error_log, String
+  # key :alt, String
+  # key :caption, String
+  # key :created_at, String
+  # key :updated_at, String
+
   has_many :product_images, dependent: :destroy
   has_many :products, through: :product_images, source: :product
 
@@ -11,5 +21,51 @@ class Image < ApplicationRecord
   has_many :cdn_files
   alias_attribute :files, :cdn_files
 
-  validates :alt, presence: true, uniqueness: true
+  # The user who uploaded the image.
+  belongs_to :user, optional: true
+
+  # Too zealous
+  # validates :alt, presence: true, uniqueness: true
+
+  # Videos can have many sizes and file types.
+  # This method collects all related CdnFile and concatenates their path endings into 1 string.
+  # This string is then stored on Image.variations.
+  # def sync_variations
+  #   variations = cdn_files.map { |f| f.path.scan('@').any? ? f.path.split('@').last : f.path.split('.').last }.join(',')
+  #   update(variations: variations)
+  # end
+
+  # Life cycle hooks
+  after_create :convert_image
+  # after_update :convert_image
+  # after_destroy :delete_image
+
+  def convert_image
+    ConvertImageJob.perform_later self
+  end
+
+  # def delete_image
+  #   DeleteImageJob.perform_later self
+  # end
+
+  def file_name
+    path.split('/').last
+  end
+
+  def original_width
+    original.split('x').first.gsub('@', '').to_i
+  end
+
+  def original_ext
+    original.split('.').last
+  end
+
+  def ratio
+    return nil if original.nil?
+
+    sizes = original.split('.').first.gsub('@', '').split('x')
+    width = sizes.first.to_i
+    height = sizes.last.to_i
+    (width / height).to_f
+  end
 end
